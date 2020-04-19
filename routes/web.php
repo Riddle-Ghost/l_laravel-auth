937ccb6ba97529c5728c924fd1298f97f3cfea76
Riddle-Ghost <test@test.com>
2020-04-19 14:04:57 +0300

    add web routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,35 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', 'HomeController@index');
+
+Route::group(['middleware' => 'guest'], function() {
+
+    Route::group(['namespace' => 'Auth'], function() { 
+
+        Route::get('/login', 'LoginController@showLoginForm')->name('login');
+        Route::post('/login', 'LoginController@login');
+
+        Route::get('register', 'RegisterController@showRegistrationForm')->name('register');
+        Route::post('register', 'RegisterController@register');
+
+        Route::get('email/verify', 'VerificationController@show')->name('verification.notice');
+        Route::get('email/verify/{id}', 'VerificationController@verify')->name('verification.verify');
+        Route::get('email/resend', 'VerificationController@resend')->name('verification.resend');
+        Route::get('/verify/{token}', 'RegisterController@verify')->name('register.verify');
+    });
+    
 });
+
+Route::group(['middleware' => 'auth'], function() {
+
+    Route::group(['namespace' => 'Auth'], function() { 
+
+        Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+    });
+});
+
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
