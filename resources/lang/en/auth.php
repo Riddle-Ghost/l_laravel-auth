3bf086b975d91b63d591e39797468f666da982c3
Riddle-Ghost <test@test.com>
2020-04-23 19:59:07 +0300

    translate

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Неверные данные',
+    'throttle' => 'Слишком много попыток. Попробуйте снова через :seconds секунд.',
 
 ];
