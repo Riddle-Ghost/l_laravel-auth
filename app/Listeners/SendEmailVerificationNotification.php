e4bab1d9f5f609ee906e5a2d361bbc08db83f628
Riddle-Ghost <test@test.com>
2020-04-23 20:38:54 +0300

    add listener

---
+++ app/Listeners/SendEmailVerificationNotification.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class SendEmailVerificationNotification
+{
+
+    /**
+     * Handle the event.
+     *
+     * @param  \Illuminate\Auth\Events\Registered  $event
+     * @return void
+     */
+    public function handle(Registered $event)
+    {
+        if (! $event->user->hasVerifiedEmail()) {
+            $event->user->sendEmailVerificationNotification();
+        }
+    }
+}
