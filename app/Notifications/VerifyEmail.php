a9027a91aa3a27de534474b473089ec2a70260b3
Riddle-Ghost <test@test.com>
2020-04-24 18:42:46 +0300

    some changes

--- app/Notifications/VerifyEmail.php
+++ app/Notifications/VerifyEmail.php
@@ -5,8 +5,6 @@ namespace App\Notifications;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-use Illuminate\Support\Carbon;
-use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Notifications\Notification;
 use Illuminate\Notifications\Messages\MailMessage;
