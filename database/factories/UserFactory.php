f30600336b104567596199c4eeb8eb6e7db1ecdb
Riddle-Ghost <test@test.com>
2020-04-24 18:44:37 +0300

    add const

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,7 +24,7 @@ $factory->define(User::class, function (Faker $faker) {
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
         'avatar' => '',
-        'role' => 0,
-        'status' => 0,
+        'role' => USER::USER,
+        'status' => USER::NORMAL,
     ];
 });
